d04745a1a2a24356a8e6662fc8683986abe27736
Enigma <sevoalex@mail.ru>
2022-05-22 08:54:29 +0300

    podgotovka proecta

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div> Здесь будет приложение </div>
+    )
+  }
+}
+
+export default Header
